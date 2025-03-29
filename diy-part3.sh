2e4ac6ac81b1c908f6876de9fabe0e3c1e350d87
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-03-29 20:25:47 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -17,8 +17,11 @@
  sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 将构建日期添加到概览页面
- sed -i 's/%D %V/%D %V | Build by Blacknesswing |/g' package/base-files/files/usr/lib/os-release
+ # sed -i 's/%D %V/%D %V | Build by Blacknesswing |/g' package/base-files/files/usr/lib/os-release
  # sed -i "s/%C/($(date +"%Y-%m-%d"))/g" package/base-files/files/usr/lib/os-release
+ sed -i "s/%D %V/%D %V | Build by Blacknesswing | Compiled on $(date '+%Y-%m-%d') |/g" package/base-files/files/usr/lib/os-release
+
+
 
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
 # sed -i 's/KERNEL_PATCHVER=6.6/KERNEL_PATCHVER=6.11/g' target/linux/x86/Makefile  #修改内核版本
