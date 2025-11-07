ba9f7fc0f25ce610ac9250423826d34e1702a298
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-11-07 11:27:31 +0800

    Change model name from 'X86 Software Router' to 'X86 Router'

--- diy-part3.sh
+++ diy-part3.sh
@@ -49,8 +49,8 @@ boot() {
     model="$(cat /sys/class/dmi/id/product_name 2>/dev/null)"
     if [ -z "$model" ] || echo "$model" | grep -qi "default string"; then
         mkdir -p /tmp/sysinfo
-        echo "X86 Software Router" > /tmp/sysinfo/model
-        logger -t fix-x86-model "BIOS 未提供主板信息，型号已设为 X86 Software Router"
+        echo "X86 Router" > /tmp/sysinfo/model
+        logger -t fix-x86-model "BIOS 未提供主板信息，型号已设为 X86 Router"
     else
         logger -t fix-x86-model "检测到主板型号: $model，保持原样"
     fi
